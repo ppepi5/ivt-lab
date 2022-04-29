0837b217ac98ce5d41af90c5c9e30cb08a82237a
ppepi5 <ppetra76@gmail.com>
2022-04-29 11:40:34 +0200

    blank line added

--- src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
+++ src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
@@ -13,6 +13,7 @@ public class TorpedoStore {
   private double FAILURE_RATE = 0.0; //NOSONAR
 
   private int torpedoCount = 0;
+  
   Random generator = new Random();
 
   public TorpedoStore(int numberOfTorpedos){
